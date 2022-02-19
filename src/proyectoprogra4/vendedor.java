c58015b91a3470f9febae68bab048adf061b6233
chenach <edwindanielpineda05@gmail.com>
2022-02-18 19:41:24 -0600

    cambio de ubicacion

---
+++ src/proyectoprogra4/vendedor.java
@@ -0,0 +1,16 @@
+
+package proyectoprogra4;
+
+
+public class vendedor extends usuario {
+    private String Password;
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    
+}
