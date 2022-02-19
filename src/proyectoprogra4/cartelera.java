759bbd9193609a3c61502d3a2d55e597f7c985a6
AlisonCeleste98 <acpo98@outlook.com>
2022-02-19 17:23:14 -0600

    Clase cartelera

---
+++ src/proyectoprogra4/cartelera.java
@@ -0,0 +1,46 @@
+
+package proyectoprogra4;
+
+
+public class cartelera {
+    private String Peliculas;
+    private String Horarios;
+    private String TipoSala;
+    private String Idioma;
+
+    
+    public String getPeliculas() {
+        return Peliculas;
+    }
+
+    public void setPeliculas(String Peliculas) {
+        this.Peliculas = Peliculas;
+    }
+
+    public String getHorarios() {
+        return Horarios;
+    }
+
+    public void setHorarios(String Horarios) {
+        this.Horarios = Horarios;
+    }
+
+    public String getTipoSala() {
+        return TipoSala;
+    }
+
+    public void setTipoSala(String TipoSala) {
+        this.TipoSala = TipoSala;
+    }
+
+    public String getIdioma() {
+        return Idioma;
+    }
+
+    public void setIdioma(String Idioma) {
+        this.Idioma = Idioma;
+    }
+    
+    
+    
+}
